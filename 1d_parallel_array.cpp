039571d1b03a14935518903afeb43e0487fa80c1
sakairi-nobuyuki <SAKAIRI.Nobuyuki@gmail.com>
2020-06-29 01:04:13 +0900

    everything was wrong. non neccesary codes are deleted with deep regrets.

--- 1d_parallel_array.cpp
+++ 1d_parallel_array.cpp
@@ -27,7 +27,7 @@ parallelArray1D::parallelArray1D () {
     kB = 1.0;
     T  = 100;
     Beta = 1.0 / (kB * T);
-    Z  = obtainDistributionFunction ();
+
 
     //  allocate spin and spin record book
     s.resize (n_particles);
@@ -38,66 +38,6 @@ parallelArray1D::parallelArray1D () {
     initRandomSpins (); 
 
 }
-
-
-double parallelArray1D::obtainDistributionFunction () {
-    int i, j, k, spin_status_in_bin, spin_target[n_particles], spin_test;
-    const char* spin_state_char;
-    string spin_state, spin_state_1_digit;
-    list<string> all_spin_state;
-    list<string>::iterator all_spin_state_itr;
-    vector<int> spin_subject (n_particles);
-    double Z, H;
-
-    Z = 0.0;
-    H = 0.0;
-    
-    setAllSpinState (all_spin_state);
-    
-    //  Z = sum exp (-Beta H (s))
-    //cout << endl << endl << "all spinstate" << endl;
-    for (all_spin_state_itr = all_spin_state.begin (); all_spin_state_itr != all_spin_state.end (); all_spin_state_itr++) {
-        spin_state = *all_spin_state_itr;    //  transform list of string to string
-        //cout << "spin state: " << spin_state << ", size of spin state = " << spin_state.size () << endl;    
-        for (i = 0; i < spin_state.size (); i++) {
-            spin_state_1_digit = spin_state[i];
-            //cout << spin_state_1_digit << " " << spin_state[i] << endl;
-            spin_test = stoi (spin_state_1_digit);
-            if (spin_test == 0) spin_test = -1;
-            //cout << spin_test << endl;
-            spin_subject[i] = spin_test;
-            cout << spin_subject[i];
-        }
-        cout << endl;
-        
-        H = obtainHamiltonian (spin_subject);
-        Z += exp (-Beta * H);
-
-        cout << "H = " << H << endl;
-        
-    }
-    
-    cout << "Z = " << Z << endl;
-    
-    return Z;
-}
-
-
-void parallelArray1D::setAllSpinState (list<string>& AllSpinState) {
-    int i;
-
-    for (i = 0; i < pow (n_status, n_particles); i++) {
-        string spin_status_in_str;
-        stringstream bin_in_string;    
-
-        bin_in_string << bitset <sizeof (i) * __CHAR_BIT__> (i);
-        spin_status_in_str = bin_in_string.str ().substr (sizeof (i) * __CHAR_BIT__ - n_particles, n_particles);
-        cout << i << " " << spin_status_in_str << endl;
-        AllSpinState.push_back (spin_status_in_str);
-    }
-    cout << "all spin set finished" << endl;
-}
-
 
 void parallelArray1D::initRandomSpins () {
     for (i = 0; i <n_particles; i++) {
@@ -121,7 +61,8 @@ void parallelArray1D::perturbSpinDistributionWithGibbsSampling () {
         i_sample = rand () % n_particles;
         cout << i_sample << "shall be evaluated first" << endl;
         if (s_record_book[i_sample] == 0) {
-            Peval = exp (-Beta * obtainHamiltonian (s)) / Z;
+            
+            Peval = 0.0;
             Prand = (double) (rand () % 100) * 0.01;
             if (Peval < Prand) {
                 s[i_sample] = 1;
@@ -140,49 +81,11 @@ void parallelArray1D::perturbSpinDistributionWithGibbsSampling () {
 
 }
 
-double parallelArray1D::obtainHamiltonian (vector<int>& s) {
-    int i;
-    double H;
 
-    // boundary condition
-    //cout << "set cyclic boundary condition with spin state ";
-    //for (i = 0; i < n_particles; i++) cout << s[i];
-    //cout << endl;
-    //cout << "finished to set cyclic boundary condition" << endl;
-
-    //  calculating Hamiltonian
-    //cout << "start to calculate Hamiltonian" << endl;
-    H = 0.0;
-    //cout << "initialize Hamiltonian" << endl;
-    for (i = 0 ; i < s.size (); i++) {
-        for (k = 0; k < i; k++) {
-            J = obtainEDLInteractionDiscrete1D (i, k) * s[i] * s[k]
-                - obtainVanDerWaalsInteractionDiscrete1D (i, k);
-            H += J;
-            cout << "i = " << i << ", j = " << k << ", s_i, s_k = " << s[i] << ", " << s[k] << ", J = " << J << ", p = " << exp (-Beta * J) << ", Z = " << Z << endl;
-        }
-    }
-    
-    
-    return H;
-}
-
-double parallelArray1D::obtainProbability (double H) {
-    return exp (-1.0 * Beta * H) / Z;
-}
 
 void parallelArray1D::obtainCyclicBoundaryCondition () {
     //  cyclic boundary condition 
     s[0] = s[n_particles - 2];
-}
-
-
-void parallelArray1D::printHamiltonianAndSpinStatus () {
-    //cout << "Hamiltonian: " << H << endl;
-    cout << "Spin status:" << endl;
-    for (i = 0; i <n_particles; i++) cout << s[i] << " ";
-    cout << endl;
-    cout << "End spin status:" << endl;
 }
 
 
--- 1d_parallel_array.h
+++ 1d_parallel_array.h
@@ -10,7 +10,7 @@ class parallelArray1D {
         int i, j, k, l;
         double Jedl, JvdW, J;    //  Hamiltonian, interaction terms
         double H, Kappa, CvdW, d;   //  width of EDL, coeffient of van der Waals force
-        double Z, Beta, kB, T;      //  Beta = kB T, kB: Boltzmann factor, T: temperature
+        double Beta, kB, T;      //  Beta = kB T, kB: Boltzmann factor, T: temperature
         
         vector<int> s, s_record_book;
         vector<string> s_all_state_in_str;
@@ -23,8 +23,6 @@ class parallelArray1D {
         double obtainD1Distance (int i, int j);
 
 
-        void setAllSpinState (list<string>& AllSpinState);
-
         double obtainEDLInteraction ();
         double obtainVanDerWaalsInteraction ();
 
@@ -33,14 +31,11 @@ class parallelArray1D {
         double obtainD2DistanceDiscrete1D (int x1, int x2);    
         double obtainEDLInteractionDiscrete1D (int x1, int x2);
         double obtainVanDerWaalsInteractionDiscrete1D (int x1, int x2);
-        double obtainDistributionFunction ();
+        
         double obtainProbability (double H);
     public:
         parallelArray1D ();
-        
-        //double obtainHamiltonian (int *s, int n_particles);
-        double obtainHamiltonian (vector<int>& s);
-        void printHamiltonianAndSpinStatus ();
+                
         void perturbSpinDistributionWithGibbsSampling ();
 
 
